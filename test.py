ce040e4c518387858f4f2c82aa173726181546cb
rmike <ya.m2soft@yandex.ru>
2019-06-09 19:32:38 +0300

    del all

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import subprocess
-subprocess.check_output('mkdir ./testDir', shell=True)
-subprocess.check_output('cp ./psd ./testDir -R', shell=True)
-subprocess.check_output('cd  testDir', shell=True)
-subprocess.check_output('mkdir ./test1', shell=True)
-subprocess.check_output('mkdir ./test2', shell=True)
-subprocess.check_output('mkdir ./test3', shell=True)
-print ("test")
